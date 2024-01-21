7e58904fbe268b9a2a893aa16c88e52cb157c174
mrlulu51 <lulubinou51@gmail.com>
2024-01-21 17:18:27 +0100

    Finished some configurations in the root of project

---
+++ dist/users/users.routes.config.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UsersRoutes = void 0;
+const common_routes_config_1 = require("../common/common.routes.config");
+class UsersRoutes extends common_routes_config_1.CommonRoutesConfig {
+    constructor(app) {
+        super(app, 'UsersRoutes');
+    }
+    configureRoutes() {
+        return this.app;
+    }
+}
+exports.UsersRoutes = UsersRoutes;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlcnMucm91dGVzLmNvbmZpZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy91c2Vycy91c2Vycy5yb3V0ZXMuY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUVBLHlFQUFvRTtBQUVwRSxNQUFhLFdBQVksU0FBUSx5Q0FBa0I7SUFDL0MsWUFBWSxHQUF3QjtRQUNoQyxLQUFLLENBQUMsR0FBRyxFQUFFLGFBQWEsQ0FBQyxDQUFBO0lBQzdCLENBQUM7SUFFRCxlQUFlO1FBQ1gsT0FBTyxJQUFJLENBQUMsR0FBRyxDQUFDO0lBQ3BCLENBQUM7Q0FDSjtBQVJELGtDQVFDIn0=
