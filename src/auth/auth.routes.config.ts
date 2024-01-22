f33ba1c0522aa4976d04e8ade4bb0925b8e36d74
mrlulu51 <lulubinou51@gmail.com>
2024-01-22 11:21:27 +0100

    Finished JWT refreshToken system

--- src/auth/auth.routes.config.ts
+++ src/auth/auth.routes.config.ts
@@ -4,6 +4,7 @@ import { body } from "express-validator";
 import { CommonRoutesConfig } from "../common/common.routes.config";
 import authController from "./controllers/auth.controller";
 import authMiddleware from "./middleware/auth.middleware";
+import jwtMiddleware from './middleware/jwt.middleware';
 import BodyValidationMiddleware from "../common/middleware/body.validation.middleware";
 
 export class AuthRoutes extends CommonRoutesConfig {
@@ -17,6 +18,13 @@ export class AuthRoutes extends CommonRoutesConfig {
             body('password').isString(),
             BodyValidationMiddleware.verifyBodyFieldsErrors,
             authMiddleware.verifyUserPassword,
+            authController.createJWT,
+        ]);
+
+        this.app.post('/auth/refresh-token', [
+            jwtMiddleware.validJWTNeeded,
+            jwtMiddleware.verifyRefreshBodyField,
+            jwtMiddleware.validRefreshNeeded,
             authController.createJWT,
         ]);
 
